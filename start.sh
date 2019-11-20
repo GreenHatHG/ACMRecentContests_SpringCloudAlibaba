a572c491aaee2cc66abd9632674600bd38127e58
GreenHatHG <1239776759@qq.com>
2019-11-20 09:51:55 +0800

    up travis.conf

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 ip=$1
 pwd=$2
 
-for arg; 
-do
-    if [ $arg==$ip ]||[ $arg==$pwd ];
-    then
-        continue
-    fi
-    mvn clean install -f $arg/pom.xml
-done
+# for arg; 
+# do
+#     if [ $arg==$ip ]||[ $arg==$pwd ];
+#     then
+#         continue
+#     fi
+#     mvn clean install -f $arg/pom.xml
+# done
 
 # -----------------创建dockerfile--------------------- 
 # $1:项目名，$2:jar的相对路径
@@ -19,13 +19,12 @@ createDokcerfiler(){
     touch $1/dockerfile
     echo 'FROM openjdk:8-alpine' >> $1/dockerfile
     echo 'RUN mkdir /app' >> $1/dockerfile
-    echo 'COPY target/'$2 ' /app/' >> $1/dockerfile
+    echo 'COPY '$1'/target/'$2 ' /app/' >> $1/dockerfile
     echo 'CMD java -jar /app/'$2 >> $1/dockerfile
 }
 
 # -----------------打包--------------------- 
 baseFolder='/root/spring-cloud-alibaba/'
-mkdirCommand='mkdir -p '+baseFolder
 
 for path in `ls`
 do
@@ -40,10 +39,10 @@ do
     done    
     # 判断是不是文件夹
     if [ $flag == 1 ]&&[ -d $path/target ]; then
-        mvn clean package -f $path/pom.xml
+        # mvn clean package -f $path/pom.xml
         # 创建文件夹用来存放jar
-        sshpass -p $pwd ssh -o StrictHostKeyChecking=no root@$ip mkdirCommand+$path || true
-        sshpass -p $pwd scp $path/target/*.jar root@$ip:baseFolder+$path
+        # sshpass -p $pwd ssh -o StrictHostKeyChecking=no root@$ip 'mkdir -p '$baseFolder$path || true
+        # sshpass -p $pwd scp $path/target/*.jar root@$ip:baseFolder+$path
         jarPath=`ls $path/target | grep jar`
         createDokcerfiler $path $jarPath
     fi

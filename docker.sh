9dabcf5725ea070f93964046929c47f99089f72c
GreenHatHG <1239776759@qq.com>
2019-11-20 09:39:17 +0800

    up travis.conf

--- docker.sh
+++ docker.sh
@@ -6,6 +6,7 @@ for path in `ls`
 do
     if [ -f $path/dockerfile ]; then
         docker build -t $path:$time $path
+        docker tag $path:$time registry.cn-hongkong.aliyuncs.com/acm-recentcontests/$path:$time
         docker push registry.cn-hongkong.aliyuncs.com/acm-recentcontests/$path:$time
     fi    
 done
